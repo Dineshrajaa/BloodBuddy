43881ac0f37250c7585e0f75e55549f2b0e2b409
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-03-03 18:53:53 +0530

    Added Facebook Authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Component, ViewChild} from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar, Splashscreen, NativeStorage } from 'ionic-native';
 
 import { LandingPage } from '../pages/landing/landing';
 import { HomePage } from '../pages/home/home';
@@ -10,14 +10,28 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = LandingPage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      // Check whether the user has already authourized the app via Facebook
+      let env=this;
+      NativeStorage.getItem('user')
+     .then(function (data) {
+        // user is previously logged and we have his data
+        // we will let him access the app
+        console.warn(JSON.stringify(data));
+        env.nav.push(HomePage);
+        Splashscreen.hide();
+      }, function (error) {
+        //we don't have the user data so we will ask him to log in
+        console.warn(JSON.stringify(error));
+        env.nav.push(LandingPage);
+        Splashscreen.hide();
+      });
+
       StatusBar.styleDefault();
-      Splashscreen.hide();
     });
   }
 }
--- src/pages/landing/landing.ts
+++ src/pages/landing/landing.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Facebook, NativeStorage } from 'ionic-native';
+import { HomePage } from '../home/home';
 /*
   Generated class for the Landing page.
 
@@ -12,8 +13,44 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'landing.html'
 })
 export class LandingPage {
+	FB_APP_ID: number = 1831487603792092;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	Facebook.browserInit(this.FB_APP_ID, "v2.8");
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  doFbLogin(){
+    let permissions = new Array();
+    let nav = this.navCtrl;
+    //the permissions your facebook app needs from the user
+    permissions = ["public_profile"];
+
+
+    Facebook.login(permissions)
+    .then(function(response){
+      let userId = response.authResponse.userID;
+      let params = new Array();
+
+      //Getting name and gender properties
+      Facebook.api("/me?fields=name,gender", params)
+      .then(function(user) {
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+        //now we have the users info, let's save it in the NativeStorage
+        NativeStorage.setItem('user',
+        {
+          name: user.name,
+          gender: user.gender,
+          picture: user.picture
+        })
+        .then(function(){
+          nav.push(HomePage);
+        }, function (error) {
+          console.log(error);
+        })
+      })
+    }, function(error){
+      console.log(error);
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LandingPage');
